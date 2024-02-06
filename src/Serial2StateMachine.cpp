f19143f720c9f730aad41be44166b9457a30b252
Alessio Igor Bogani <alessio.bogani@elettra.eu>
2024-02-06 11:35:18 +0100

    Allow commands execution on FAULT state

--- src/Serial2StateMachine.cpp
+++ src/Serial2StateMachine.cpp
@@ -114,8 +114,7 @@ bool Serial2::is_Reconnections_allowed(TANGO_UNUSED(Tango::AttReqType type))
 bool Serial2::is_Write_allowed(TANGO_UNUSED(const CORBA::Any &any))
 {
 	//	Compare device state with not allowed states.
-	if (get_state()==Tango::FAULT ||
-		get_state()==Tango::INIT)
+	if (get_state()==Tango::INIT)
 	{
 	/*----- PROTECTED REGION ID(Serial2::WriteStateAllowed) ENABLED START -----*/
 	
@@ -134,8 +133,7 @@ bool Serial2::is_Write_allowed(TANGO_UNUSED(const CORBA::Any &any))
 bool Serial2::is_Read_allowed(TANGO_UNUSED(const CORBA::Any &any))
 {
 	//	Compare device state with not allowed states.
-	if (get_state()==Tango::FAULT ||
-		get_state()==Tango::INIT)
+	if (get_state()==Tango::INIT)
 	{
 	/*----- PROTECTED REGION ID(Serial2::ReadStateAllowed) ENABLED START -----*/
 	
@@ -154,8 +152,7 @@ bool Serial2::is_Read_allowed(TANGO_UNUSED(const CORBA::Any &any))
 bool Serial2::is_ReadUntil_allowed(TANGO_UNUSED(const CORBA::Any &any))
 {
 	//	Compare device state with not allowed states.
-	if (get_state()==Tango::FAULT ||
-		get_state()==Tango::INIT)
+	if (get_state()==Tango::INIT)
 	{
 	/*----- PROTECTED REGION ID(Serial2::ReadUntilStateAllowed) ENABLED START -----*/
 	
